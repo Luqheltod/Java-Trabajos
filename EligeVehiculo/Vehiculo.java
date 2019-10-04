f2fe0a54d002327b22219a84ee1931209ab236ef
German Sanchez <45257012+Luqheltod@users.noreply.github.com>
2019-10-04 19:14:57 +0200

    Update Vehiculo.java

--- EligeVehiculo/Vehiculo.java
+++ EligeVehiculo/Vehiculo.java
@@ -5,7 +5,9 @@ package VehiculosAbstract;
  *
  * @author Gsancho
  */
- public abstract class Vehiculo {
+ public abstract class Vehiculo { //se declaran las variables que tendran los demas objetos, el Idsiguiente al ser static
+                                  //sirve como comodin para sumar uno cada vez que se crea un vehiculo, se suma uno y se añade +
+                                  // la copia ya sumada al crear el objeto.
     
    protected String color;
    protected String categoria;
@@ -22,19 +24,19 @@ package VehiculosAbstract;
         
     };
 
-    public String getColor() {
+    public String getColor() { //el metodo getcolor comun para todas las demas clases 
         return color;
     }
 
-    public int getId() {
+    public int getId() { //el metodo getId comun para todas las demas clases 
         return id;
     }
 
    
     
-    public abstract String datosVehiculo();
+    public abstract String datosVehiculo();  // el metodo que han de sobreescribir todas nuestras demas clases que hereden de esta.
     
-    public void setCategoria(int x){
+    public void setCategoria(int x){ //este metodo nunca se usa, pero era una idea previa para jugar con las categorias con base int.
         
         if(x==1){
             
@@ -50,7 +52,7 @@ package VehiculosAbstract;
         
     }
     
-    public String getCategoria(){
+    public String getCategoria(){  // un simple metodo para usar en la descripcion, se podrian haber escrito asi todas las partes de la descripcion.
         
         
         return "La categoria del vehiculo es: " +categoria ;
